dcbf57874f08c800982cf299402223a8b408df5f
Trump-2 <akashidaiki2@gmail.com>
2024-06-06 21:34:52 +0800

    lec23 ( unfinished )

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,18 +4,20 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Validation\Rule;
 
 class UserController extends Controller
 {
     // 在函數中添加 Request $request，來取得 form 表單提交後傳送的所有資料
     public function register(Request $request)
     {
-        // $request->validate() 用來制定表單欄位的各種規則
+        // $request->validate() 用來制定表單欄位的各種驗證規則
         $incomingFields = $request->validate(
             [
-                'username' => 'required',
-                'email' => 'required',
-                'password' => 'required'
+                // 這裡使用內建的 Rule class 的 unique method；第一個參數是資料庫中的某資料表名稱，第二個參數是該資料表中的某一個欄位名稱
+                'username' => ['required', 'min:3', 'max:20', Rule::unique('users', 'username')],
+                'email' => ['required', 'email', Rule::unique('users', 'email')],
+                'password' => ['required', 'min:8', 'confirmed']
             ]
         );
         // 使用 User Model 將表單送來的資料存入資料庫中
