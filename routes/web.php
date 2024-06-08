cbb1955c8cb9e3a56153ecfdbf4e195d78a05d71
Wu-YingHong <akashidaiki2@gmail.com>
2024-06-08 16:40:22 +0800

    lec26 ( unfinished )

--- routes/web.php
+++ routes/web.php
@@ -2,12 +2,15 @@
 
 use Illuminate\Support\Facades\Route;
 // import 我們建立的 controller
+use App\Http\Controllers\PostController;
 use App\Http\Controllers\UserController;
 
+// User related routes
 Route::get('/', [UserController::class, 'showCorrectHomepage']);
-
-
 Route::post('/register', [UserController::class, 'register']);
-
 Route::post('/login', [UserController::class, 'login']);
 Route::post('/logout', [UserController::class, 'logout']);
+
+// Blog post related routes
+Route::get('/create-post', [PostController::class, 'showCreateForm']);
+Route::post('/create-post', [PostController::class, 'storeNewPost']);
