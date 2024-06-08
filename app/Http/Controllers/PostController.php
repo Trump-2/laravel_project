4743b80a39f7bfe1b388b58092842af2816bff35
Trump-2 <akashidaiki2@gmail.com>
2024-06-08 21:30:54 +0800

    lec26

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,13 +2,27 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Post;
 use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
     //
-    public function storeNewPost()
+    public function storeNewPost(Request $request)
     {
+        $incomingFields = $request->validate([
+            'title' => 'required',
+            'content' => 'required',
+        ]);
+
+        // 使用 php 的 strip_tags ( ) 對使用者提供的資料進行處理
+        $incomingFields['title'] = strip_tags($incomingFields['title']);
+        $incomingFields['content'] = strip_tags($incomingFields['content']);
+
+        // 從 session 中得到登入使用者所擁有的 id，透過使用內建的【auth(  ) -> id ( )】
+        $incomingFields['user_id'] = auth()->id();
+
+        Post::create($incomingFields);
     }
 
     public function showCreateForm()
--- database/migrations/2024_06_08_063521_create_posts_table.php
+++ database/migrations/2024_06_08_063521_create_posts_table.php
@@ -14,8 +14,13 @@ return new class extends Migration
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
             $table->string('title');
+            // longText( ) 用於在資料表中建立大型文字欄位。與 string() 相比，longText() 可以儲存更多的文字
             $table->longText('content');
-            $table->foreignId('user_id');
+
+            // 使用到 【foreignId ( )】 這個資料類型，它的參數【 user_id 】代表名為「User」的 model、代表名為 「id」的欄位
+            // constrained () 用來確保 foreign key 的值一定存在於參考的資料表中
+            // 【onDelete ( )】：如此參考的資料表內的某筆資料被刪除時，和該筆資料相關聯的資料也會跟著一起被刪除
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
             $table->timestamps();
         });
     }
