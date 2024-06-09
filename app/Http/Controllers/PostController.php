9cc8a16cae41fe487408c572b2b2044c15277507
Wu-YingHong <akashidaiki2@gmail.com>
2024-06-09 16:39:52 +0800

    lec30 (unfinished)

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -41,6 +41,7 @@ class PostController extends Controller
 
     public function showCreateForm()
     {
+
         return view('create-post');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -4,14 +4,18 @@ use Illuminate\Support\Facades\Route;
 // import 我們建立的 controller
 use App\Http\Controllers\PostController;
 use App\Http\Controllers\UserController;
+use App\Http\Middleware\MustBeLoggedIn;
 
 // User related routes
-Route::get('/', [UserController::class, 'showCorrectHomepage']);
-Route::post('/register', [UserController::class, 'register']);
-Route::post('/login', [UserController::class, 'login']);
-Route::post('/logout', [UserController::class, 'logout']);
+Route::get('/', [UserController::class, 'showCorrectHomepage'])->name('login');
+// 只有訪客可以訪問 /register，所以使用 middleware('guest')
+Route::post('/register', [UserController::class, 'register'])->middleware('guest');
+// 只有訪客可以訪問 /login，所以使用 middleware('guest')
+Route::post('/login', [UserController::class, 'login'])->middleware('guest');
+// 只有登入者可以訪問 /logout，所以使用 middleware('guest')
+Route::post('/logout', [UserController::class, 'logout'])->middleware('auth');
 
 // Blog post related routes
-Route::get('/create-post', [PostController::class, 'showCreateForm']);
-Route::post('/create-post', [PostController::class, 'storeNewPost']);
+Route::get('/create-post', [PostController::class, 'showCreateForm'])->middleware('mustBeLoggedIn');
+Route::post('/create-post', [PostController::class, 'storeNewPost'])->middleware('auth');
 Route::get('/post/{id}', [PostController::class, 'viewSinglePost']);
