868f8328124c46355a44edd76f2810ff62b34834
Trump-2 <akashidaiki2@gmail.com>
2024-06-18 19:59:42 +0800

    lec50

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,3 +1,10 @@
 import './bootstrap';
 
-alert('This is a test 123');
+import Search from './live-search';
+
+// 如果頁面上存在該搜尋 icon ( 帶有這個 class ) 才想要實體化 js class Search 的物件
+if(document.querySelector(".header-search-icon")) {
+    new Search();
+}
+
+alert("this is a message!!!")
